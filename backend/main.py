bf619655054b5a6cf34bf3904f99d09c649e5abb
Julian Boronat <julian.boronat@mongodb.com>
2024-12-02 10:49:17 +0100

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import APIRouter
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http://localhost:3000",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+router = APIRouter()
+
+@app.get("/")
+async def read_root(request: Request):
+    return {"message":"Server is running"}
