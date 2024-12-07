55f394ecb55406c1daa02e6d8b5546498752cab1
Julian Boronat <julian.boronat@mongodb.com>
2024-12-07 18:57:35 +0100

    Adding fonts plus changing Home
    Adding fonts plus changing Home

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -2,42 +2,14 @@
 
 // layout.js
 
-import { useEffect } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { GeistSans } from "geist/font/sans";
-import Login from '@/components/Login/Login';
-import Chatbot from '@/components/Chatbot/Chatbot';
-import Header from '@/components/Header/Header';
-import AccountsCards from '@/components/AccountsCards/AccountsCards';
 
 export default function RootLayout({ children }) {
-
-  useEffect(() => {
-    // Retrieve data from localStorage
-    const selectedUser = localStorage.getItem('selectedUser');
-    const activeAccountsString = localStorage.getItem('active_accounts');
-    const recentTransactionsString = localStorage.getItem('recent_transactions');
-
-    // Parse the JSON strings
-    const activeAccounts = activeAccountsString ? JSON.parse(activeAccountsString) : { accounts: [] };
-    const recentTransactions = recentTransactionsString ? JSON.parse(recentTransactionsString) : { transactions: [] };
-
-    // Log the data to the console
-    console.log("Selected User:", selectedUser);
-    console.log("Active Accounts:", activeAccounts);
-    console.log("Number of Accounts:", activeAccounts.accounts.length);
-    console.log("Recent Transactions:", recentTransactions);
-    console.log("Number of Recent Transactions:", recentTransactions.transactions.length);
-  }, []);
-
   return (
     <html lang="en" className={GeistSans.className}>
       <body>
-        {children}
-        <Login />
-        <Header />
-        <AccountsCards />
-        <Chatbot />
+          {children}
       </body>
     </html>
   );
