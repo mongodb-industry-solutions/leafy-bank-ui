3af8b01d5eb86a71ef8a1a1e7ed9e50215d4d5f6
ainhoamugica <ainhoa.mugica@mongodb.com>
2025-01-15 17:22:59 +0100

    Added info wizard

--- frontend/components/BankConnection/BankConnection.jsx
+++ frontend/components/BankConnection/BankConnection.jsx
@@ -9,10 +9,15 @@ import { Combobox, ComboboxOption } from '@leafygreen-ui/combobox';
 import { Body, H3 } from '@leafygreen-ui/typography';
 import styles from "./BankConnection.module.css";
 import Icon from "@leafygreen-ui/icon";
+import IconButton from "@leafygreen-ui/icon-button";
+import Tooltip from "@leafygreen-ui/tooltip";
+import InfoModal from "../InfoWizard/InfoWizard";
+
 
 // Main BankConnection component
 const BankConnection = ({ addBankAccount }) => {
     const [open, setOpen] = useState(false);
+    const [openHelpModal, setOpenHelpModal] = useState(false);
     const [selection, setSelection] = useState(null);
     const [status, setStatus] = useState('idle');
 
@@ -45,14 +50,41 @@ const BankConnection = ({ addBankAccount }) => {
 
     return (
         <div>
-            <Button onClick={() => setOpen(true)} leftGlyph={<Icon glyph="Connect" />}  className={styles.connectBtn}>Connect Bank</Button>
+            <Button onClick={() => setOpen(true)} leftGlyph={<Icon glyph="Connect" />} className={styles.connectBtn}>Connect Bank</Button>
 
             <Modal open={open} setOpen={setOpen} className={styles.modal}>
 
                 <div className={styles.popupOverlay}>
                     {status === 'idle' && (
                         <>
-                            <H3>Choose your bank</H3>
+
+                            <div className={styles.modalTitle}>
+                                <H3>Choose your bank</H3>
+                                <div className={styles.infoModal}>
+                                    <InfoModal
+                                        open={openHelpModal}
+                                        setOpen={setOpenHelpModal}
+                                        title="What is Open Finance?"
+                                        body="Open Finance refers to the concept of allowing customers to securely share their financial data with third parties, beyond traditional banking services, to enable a broader range of financial products and services. It builds upon the principles of Open Banking, which focuses primarily on bank accounts, but extends the scope to include other financial products such as investments, insurance, pensions, and loans."
+                                        tooltipText="Tell me more!"
+                                        iconGlyph="Wizard"
+                                        sections={[
+                                            {
+                                                heading: "What are we doing behind the scenes?",
+                                                image: {
+                                                    src: "/images/OF_info.png",
+                                                    alt: "Open Finance Architecture",
+                                                },
+                                            },
+                                            {
+                                                heading: "Why MongoDB?",
+                                                body: "MongoDB shines in its flexibility—serving as a central data storage solution for retrieving data from external financial institutions while seamlessly supporting diverse formats and structures.",
+                                            },
+                                        ]}
+                                    />
+                                </div>
+                            </div>
+
                             <Body>Select a bank to proceed with the connection.</Body>
 
                             <ComboboxExample
